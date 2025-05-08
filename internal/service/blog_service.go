827100cbb49ccdda2d55e283259e60b55cb94d59
artnikel <artyom.leskco@gmail.com>
2025-05-08 17:31:04 +0300

    add init handle implementation

--- internal/service/blog_service.go
+++ internal/service/blog_service.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/artnikel/blogapi/internal/model"
 	"github.com/google/uuid"
@@ -11,10 +12,10 @@ type BlogRepository interface {
 	Create(ctx context.Context, blog *model.Blog) error
 	Get(ctx context.Context, id uuid.UUID) (*model.Blog, error)
 	Delete(ctx context.Context, id uuid.UUID) error
-	DeleteByProfileID(ctx context.Context, id uuid.UUID) error
+	DeleteByUserID(ctx context.Context, id uuid.UUID) error
 	Update(ctx context.Context, blog *model.Blog) error
 	GetAll(ctx context.Context) ([]*model.Blog, error)
-	GetByProfileID(ctx context.Context, id uuid.UUID) ([]*model.Blog, error)
+	GetByUserID(ctx context.Context, id uuid.UUID) ([]*model.Blog, error)
 }
 
 type BlogService struct {
@@ -23,4 +24,60 @@ type BlogService struct {
 
 func NewBlogService(blogRps BlogRepository) *BlogService {
 	return &BlogService{blogRps: blogRps}
+}
+
+func (s *BlogService) Create(ctx context.Context, blog *model.Blog) error {
+	err := s.blogRps.Create(ctx, blog)
+	if err != nil {
+		return fmt.Errorf("Create - %w", err)
+	}
+	return nil
+}
+
+func (s *BlogService) Get(ctx context.Context, id uuid.UUID) (*model.Blog, error) {
+	blog, err := s.blogRps.Get(ctx, id)
+	if err != nil {
+		return nil, fmt.Errorf("Get - %w", err)
+	}
+	return blog, nil
+}
+
+func (s *BlogService) Delete(ctx context.Context, id uuid.UUID) error {
+	err := s.blogRps.Delete(ctx, id)
+	if err != nil {
+		return fmt.Errorf("Delete - %w", err)
+	}
+	return nil
+}
+
+func (s *BlogService) DeleteByUserID(ctx context.Context, id uuid.UUID) error {
+	err := s.blogRps.DeleteByUserID(ctx, id)
+	if err != nil {
+		return fmt.Errorf("DeleteByUserID - %w", err)
+	}
+	return nil
+}
+
+func (s *BlogService) Update(ctx context.Context, blog *model.Blog) error {
+	err := s.blogRps.Update(ctx, blog)
+	if err != nil {
+		return fmt.Errorf("Update - %w", err)
+	}
+	return nil
+}
+
+func (s *BlogService) GetAll(ctx context.Context) ([]*model.Blog, error) {
+	blogs, err := s.blogRps.GetAll(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("GetAll - %w", err)
+	}
+	return blogs, nil
+}
+
+func (s *BlogService) GetByUserID(ctx context.Context, id uuid.UUID) ([]*model.Blog, error) {
+	blogs, err := s.blogRps.GetByUserID(ctx, id)
+	if err != nil {
+		return nil, fmt.Errorf("GetByUserID - %w", err)
+	}
+	return blogs, nil
 }
